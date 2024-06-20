fe59ca99c5985f68f60e58c46ca0064f0eafb540
vineshtk <vineshtk10@gmail.com>
2024-06-20 09:50:50 +0530

    active button on navbar,

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+
+    const linkStyle = ({ isActive }) => isActive ? 'bg-purple-400 border-b p-1 rounded-md text-purple-950' : ' border-b p-1 rounded-md hover:text-purple-600'
+    
     return (
         <div className='bg-purple-100 text-purple-950 grid grid-cols-1 md:grid-cols-2 p-3 shadow-md'>
             <div className='flex items-center'>
-                
+
                 <Link to="/" >
-                <img className='m-1p-2 size-12' src="src/assets/kbalogo.png" alt="logo" />
+                    <img className='m-1p-2 size-12' src="src/assets/kbalogo.png" alt="logo" />
                 </Link>
             </div>
             <div className='flex justify-center md:justify-end items-center mt-2 md:mt-0 space-x-5 md:space-x-10'>
-                <Link to="/" className='hover:text-purple-700'>Home</Link>
-                <Link to="/courses" className='hover:text-purple-700'>Courses</Link>
-                <Link to="/contact" className='hover:text-purple-700'>Contact Us</Link>
+                <NavLink to="/" className={linkStyle}>Home</NavLink>
+                <NavLink to="/courses" className={linkStyle}>Courses</NavLink>
+                <NavLink to="/contact" className={linkStyle}>Contact Us</NavLink>
             </div>
         </div>
     );
